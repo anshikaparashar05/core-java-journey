6b27cf67160317fbbdc5b84aef7bd3fbb84dbbda
Anshika Parashar <parasharanshika178@gmail.com>
2026-03-06 23:36:41 +0530

    Strings.java

--- Strings.java
+++ Strings.java
@@ -6,7 +6,11 @@ public class InputStrings {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter your name:");
       String userName = sc.nextLine(); 
-      System.out.println("Hello, " + userName + "!");
+      System.out.println("Hello, " + userName "!");
         myObj.close();
+        /*string can be created in two ways
+        String name= "Anshika";
+        String username= new String("Anshika");
+        */
     }
 }
